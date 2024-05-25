cbfc7aea031885287610dce798b316e8c9d9d948
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-05-25 21:05:18 +0200

    added helper model class DishIngredient

--- Restaurant/Models/Dish.cs
+++ Restaurant/Models/Dish.cs
@@ -2,10 +2,10 @@
 {
     public class Dish
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
-        public double Price { get; set; }
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public double? Price { get; set; }
 
     }
 }
--- Restaurant/Models/Ingredient.cs
+++ Restaurant/Models/Ingredient.cs
@@ -2,8 +2,8 @@
 {
     public class Ingredient
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int? Id { get; set; }
+        public string? Name { get; set; }
 
     }
 }
