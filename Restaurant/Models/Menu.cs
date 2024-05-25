41bf6c919cb7e6df718b491f8af2b47fee721258
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-05-25 20:37:58 +0200

    added class Menu in Models

---
+++ Restaurant/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+
+    }
+}
