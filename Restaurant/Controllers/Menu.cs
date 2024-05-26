4930290b127161ff9fb909251c1fc4afbfbc537a
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-05-26 13:17:01 +0200

    added VieModel for MenuIndex

---
+++ Restaurant/Controllers/Menu.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Data;
+using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class Menu : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
